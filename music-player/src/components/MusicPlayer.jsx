f2680a8bb4351d56659fb0e5f7b03534d871c055
Taylor Poe <t_poe25@yahoo.com>
2026-01-20 15:38:24 -0600

    need to find out how to incoorperate

--- music-player/src/components/MusicPlayer.jsx
+++ music-player/src/components/MusicPlayer.jsx
@@ -9,15 +9,25 @@ import CoverArt from "./CoverArt";
 export default function MusicPlayer() {
   return (
     <div className="Music-Player-Big w-[896px] h-[640px] top-[-637px] left-[1460px] m-8">
-      <div className="Currently-Playing-Big w-[400px] h-[576px] top-[500px] left-[1493px]">
+      <div className="Currently-Playing-Big w-[400px] h-[576px] top-[500px] left-[1493px] border-[1px] border-#D5D7D88B bg-zinc-900 flex flex-col items-center p-4 gap-4">
+      <div className="CurrentlyPlaying" />
         <SongTitle />
-        <CoverArt />
+        <CoverArt width={400} height={400} top={-606} left={14923} />
         <PlayControls />
         <PlayListItem />
         <VolumeControls />
       </div>
-      <div className="Playlist-Big w-[427px] h-[640px] top-[-637px] left-[1908px] border-[1px] border-#D5D7D88B ">
-        {/* Add your playlist items here */}
+      <div className="PlayList-Big w-[427px] h-[640px] top-[68px] left-[1908px] border-[1px] border-color-[#D5D7D88B] bg-zinc-800 p-4 overflow-y-auto">
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
       </div>
     </div>
   );
