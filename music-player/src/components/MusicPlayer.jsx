1f0b0fb0dee8a258f06bfc5449fa3c1a1fe4ff22
Taylor Poe <t_poe25@yahoo.com>
2026-01-20 15:14:31 -0600

    trying to figure outhow to hace the currently playing and playlist side by side

--- music-player/src/components/MusicPlayer.jsx
+++ music-player/src/components/MusicPlayer.jsx
@@ -9,11 +9,16 @@ import CoverArt from "./CoverArt";
 export default function MusicPlayer() {
   return (
     <div className="Music-Player-Big w-[896px] h-[640px] top-[-637px] left-[1460px] m-8">
-      <SongTitle />
-      <CoverArt />
-      <PlayControls />
-      <PlayListItem />
-      <VolumeControls />
+      <div className="Currently-Playing-Big w-[400px] h-[576px] top-[500px] left-[1493px]">
+        <SongTitle />
+        <CoverArt />
+        <PlayControls />
+        <PlayListItem />
+        <VolumeControls />
+      </div>
+      <div className="Playlist-Big w-[427px] h-[640px] top-[-637px] left-[1908px] border-[1px] border-#D5D7D88B ">
+        {/* Add your playlist items here */}
+      </div>
     </div>
   );
 }
